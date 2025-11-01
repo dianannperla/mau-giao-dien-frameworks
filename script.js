de16dc53bda75d6b04696f7e4f61a95174721333
dianannperla <waafirmelyza@hotmail.com>
2025-11-01 09:40:49 +0000

    Updated alert message for clarity and user engagement

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('demo-button').addEventListener('click', function() {
-    alert('Thank you for interacting with our demo! We hope you enjoy it! Please provide your feedback.');
+    alert('Thank you for interacting with our demo! We hope you enjoy it. Your feedback is valuable to us!');
 });
