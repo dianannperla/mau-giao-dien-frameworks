04aafbf2ceb892a322d5d2db96168ec7988f2107
dianannperla <waafirmelyza@hotmail.com>
2025-11-01 09:40:01 +0000

    Fix alert message to improve user feedback.

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('demo-button').addEventListener('click', function() {
-    alert('Thank you for clicking the button!');
+    alert('Thank you for interacting with our demo!');
 });
